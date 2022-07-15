9c92e49cd9cffff41815c33e1288a8f8b53bc590
= <=>
2022-07-14 17:15:34 -0700

    asd

--- server.js
+++ server.js
@@ -1,14 +1,50 @@
+//#region INITIAL
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
+var path = require("path");
 var fs = require("fs");
-p = (s) => console.log(s)
+var cors = require("cors");
+var formidable = require("formidable");
+var upload = require("express-fileupload");
+var jsmediatags = require("jsmediatags");
 
-const path = String.raw`C:\Users\Lemond Wyatt\Desktop\MEWWs\This 
-is not The Ashen Demon (Embers + Inferno) from the FE 3 Hopes OST (320 kbps).mp3`;
+p = (s) => console.log(s);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var readData = JSON.parse(fs.readFileSync("./used_dir.json", "utf8"));
+var common = readData.common;
+
+//#endregion
+
+//#region FUNCTIONS
+
+function delay(secs) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(""), secs);
+  });
+}
+
+async function ak() {
+	while(true) {
+		p('hi')
+		await delay(1000)
+	}
+	
+}
+
+//ak()
+
+//#endregion
+
+//#region ROUTES
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
+app.use(upload());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -29,15 +65,120 @@ app.post("/save", (req, res) => {
   return res.end("File saved.");
 });
 
+app.post("/upload", (req, res) => {
+  var file = req.files.file;
+  var filename = file.name;
+  var path = __dirname + "/music/" + filename;
+
+  file.mv(path, (err) => {
+    if (err) res.send(err);
+    else res.send(path);
+  });
+});
+
+app.post("/dragevent", (req, res) => {
+  var names = req.body;
+	//p(names)
+  if (typeof names != "object") return p("body not a arr");
+
+	var bool = false;
+  var folders = [...common];
+  var ignore = [...readData.ignore];
+	var result = [];
+
+  p("starting search");
+
+  for (let i = 0; i < folders.length; i++) {
+    var startPath = folders[i];
+
+    if (!fs.existsSync(startPath)) {
+      console.log("no dir ", startPath);
+      continue;
+    }
+		//if (ignore.findIndex((el) => startPath.endsWith(el)) !== -1) 
+		//	continue;
+    //ignore.push(startPath);
+    var files = fs.readdirSync(startPath);
+    for (let h = 0; h < files.length; h++) {
+      const fel = files[h];
+      try {
+        var filename = path.join(startPath, fel);
+      //  p(filename);
+        var stat = fs.lstatSync(filename);
+
+        if (stat.isDirectory()) folders.push(filename);
+        else {
+					for (let j = 0; j < names.length; j++) {
+						const name = names[j];
+
+						if (filename.endsWith(name.target)) {
+							console.log("--found: ", filename);
+							names.splice(j, 1);
+							j--;
+							if (!common.includes(startPath)) common.push(startPath);
+							readData.common = common;
+							result.push({loc: name.location, newpath: filename});
+							fs.unlinkSync(name.deleteTemp);
+						}
+					}
+					if(!names) {
+						p('brokesn')
+						bool = true
+						break;
+					}
+						
+				}
+      } catch (err) {p(err)}
+			if(bool)
+				break
+    }
+		if(bool)
+				break
+		// });
+  }
+	p('search ended')
+		fs.writeFileSync("./used_dir.json", JSON.stringify(readData), (err) => {if(err) p(err)});
+		return res.send(result) 
+});
+
+app.delete("/del_path", bodyParser.text({ type: "*/*" }), (req, res) => {
+	fs.unlink(req.body, (err) => {
+    if (err) res.send(err);
+  })
+});
+
 app.get("/song/:path", (req, res) => {
-	var song_path = req.params.path.replace(/KJTGGTKGJ/g, "\\")
-	res.sendFile(song_path, (err) => {
-		if(err) {
-			console.log("ERROR: " + err);
-			return res.end("ERROR: " + err);
-		}
-	})
-	p('File Sent Successfully')
-})
+  var song_path = req.params.path.replace(/☹☸☼☺☿☾☻/g, "\\");
+  //	p('get ' + song_path)
+  res.sendFile(song_path, (err) => {
+    if (err) {
+      console.log("ERROR: " + err);
+      return res.end("ERROR: " + err);
+    }
+  });
+});
+
+app.post("/art/:path", (req, res) => {
+  var song_path = req.params.path.replace(/☹☸☼☺☿☾☻/g, "\\");
+  //consider: ☹☸☼☺☿☾☻
+
+  jsmediatags.read(song_path, {
+    onSuccess: (tag) => {
+      if (tag.tags.picture == null) return;
+      var pdata = tag.tags.picture.data;
+      var format = tag.tags.picture.format;
+      let base64String = "";
+
+      for (let i = 0; i < pdata.length; i++)
+        base64String += String.fromCharCode(pdata[i]);
+
+      //var uurl = base64String
+      return res.end(base64String);
+    },
+    onError: (err) => p(err),
+  });
+});
 
 app.listen(8000, null, () => console.log("Running"));
+
+//#endregion
